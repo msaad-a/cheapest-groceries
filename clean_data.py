199eed54de7581ffa0ae84d3c1cc0e8b198c2a53
msaad-a <msaad24@outlook.com>
2023-03-21 01:03:21 -0400

    combine data

--- clean_data.py
+++ clean_data.py
@@ -1,1 +1,16 @@
-# TODO
+
+import pandas as pd
+
+nofrills = pd.read_csv("nofrills_product_prices.csv")
+nofrills['store'] = ['nofrills'] * nofrills.shape[0]
+
+metro = pd.read_csv("metro_product_prices.csv")
+metro['store'] = ['metro'] * metro.shape[0]
+
+longos = pd.read_csv("longos_product_prices.csv")
+longos['store'] = ['longos'] * longos.shape[0]
+
+
+all_stores = pd.concat([nofrills, metro, longos], ignore_index=True)
+all_stores['product'] = all_stores['product'].apply(lambda x: x.replace('"', ''))
+all_stores.to_csv('all_product_prices.csv')
